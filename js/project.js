56ebeb3af77463967546ddf15d98e907a00dffa3
ElizaGuan <1124077632@qq.com>
2018-09-29 21:20:40 +0800

    new

--- js/project.js
+++ js/project.js
@@ -4,30 +4,38 @@ var inProject = (function () {
             ele = this.ele;
             this.$nav = this.$('.down');
             this.$menu = this.$('.menu');
-            this.$last=this.$nav.lastElementChild;
+            this.$last = this.$nav.lastElementChild;
             console.log(this.$last)
-            this.val=this.$menu.style.display;
-            this.flag=true;
+            this.val = this.$menu.style.display;
+            this.flag = true;
             this.event();
         },
         event() {
             var _this = this;
-            this.$nav.onclick = function (e) {
-                val=this.val;
-                val='none';
-                // console.log(111)
+            this.$last.onclick = function (e) {
+                e.stopPropagation();
+                val = this.val;
+                val = 'none';
                 e = e || window.event;
-                var target = e.target || e.srcElement;
-                if (target.nodeName === 'LI' && target.className === 'iconfont icon-gouwudai last_nav') {
-                    if(_this.flag){
-                        _this.$menu.style.display='block';
-                        _this.flag=false;
-                    }
-                    else{
-                        _this.$menu.style.display='none';  
-                        _this.flag=true;                              
-                    }
+                if (_this.flag) {
+                    _this.$menu.style.display = 'block';
+                    _this.flag = false;
+                    this.flag = _this.flag;
                 }
+                else {
+                    _this.$menu.style.display = 'none';
+                    _this.flag = true;
+                    this.flag = _this.flag;
+                }
+            }
+            this.$menu.onclick=function(e){
+                e.stopPropagation();
+            }
+            document.onclick = function (e) {
+                e.stopPropagation();
+                _this.$menu.style.display = 'none';
+                _this.flag = true;
+                this.flag = _this.flag;
             }
         },
         $(id) {
